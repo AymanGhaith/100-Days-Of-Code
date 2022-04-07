c43ebe6214a0a133a813c30da8e94d48879dd7a5
aymanghaith <ayman.ghaith88@gmail.com>
2022-04-07 12:27:21 +0300

    Apply Creat, insert and Deleting html elements

--- 100-Days/chapters-projects/dom_sec12/app.js
+++ 100-Days/chapters-projects/dom_sec12/app.js
@@ -1,2 +1,18 @@
-console.log(document)
-document.body.children[1].children[0].href = "https://www.google.com"
+// ADD AN ELEMENT
+// 1. Create the element
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://www.google.com";
+newAnchorElement.textContent = "Go to Google";
+
+// 2. Get access to the parent element that should holds the new element
+let firstParagraph = document.querySelector("p");
+
+// 3. Insert the new element into the parent element content
+firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENTS
+// 1. Select the element
+let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it
+firstH1Element.remove();
--- 100-Days/chapters-projects/dom_sec12/exercise.js
+++ 100-Days/chapters-projects/dom_sec12/exercise.js
@@ -3,23 +3,23 @@
 // 1. Select the <h1> element by "drilling into the DOM" and
 //    save it in a variable with a name of your choice
 
-let h1element = document.body.children[0];
-console.dir(h1element)
+let h1Element = document.body.children[0];
+console.dir(h1Element)
 
 // 2. Use the variable from (1) and get access to the "parent"
 //    element of the stored <h1> element (i.e. to the <body> element)
 //    BONUS: Try using the variable from (1) to get access to the
 //    sibling element (i.e. the <p> element next to the <h1> element)
-let h1parent = h1element.parentElement;
-console.dir(h1parent);
+let h1Parent = h1Element.parentElement;
+console.dir(h1Parent);
 
-let h1sibling = h1element.nextElementSibling;
-console.dir(h1sibling);
+let h1Sibling = h1Element.nextElementSibling;
+console.dir(h1Sibling);
 
 // 3. Select the <h1> element with getElementById and store in
 //    the same or a new variable (up to you)
-let h1elementID = document.getElementById("header");
-console.dir(h1elementID);
+let h1ElementID = document.getElementById("first-title");
+console.dir(h1ElementID);
 
 // 4. Select the second <p> element with querySelector (you might
 //    need to add something in the HTML code, e.g. a class)
